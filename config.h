9a7bc5b0f5a4b30f3c2eb81052032ca89167bbdc
Alberto Defendi <albedef@protonmail.com>
2024-09-24 09:02:14 +0200

    Apply logo patch

--- config.h
+++ config.h
@@ -4,19 +4,31 @@ static const char *group = "nobody";
 
 /* Gruvbox low-contrast */
 static const char *colorname[NUMCOLS] = {
-    [INIT] = "2d2d2d",   /* after initialization */
-    [INPUT] = "689d6a",  /* during input */
-    [FAILED] = "cc241d", /* wrong password */
+    [BACKGROUND] = "white",
+    [INIT] = "2d2d2d",    /* after initialization */
+    [INPUT] = "#005577",  /* during input */
+    [FAILED] = "#CC3333", /* wrong password */
+
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
+static const int logosize = 75;
+static const int logow =
+    12; /* Grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+    {0, 3, 1, 3}, {1, 3, 2, 1}, {0, 5, 8, 1}, {3, 0, 1, 5},  {5, 3, 1, 2},
+    {7, 3, 1, 2}, {8, 3, 4, 1}, {9, 4, 1, 2}, {11, 4, 1, 2},
+};
+
 /*Enable blur*/
 #define BLUR
-
-/*Set Blur radius*/
+/*Set blur radius*/
 static const int blurRadius = 20;
-
+/*Enable Pixelation*/
+// #define PIXELATION
 /*Set pixelation radius*/
 static const int pixelSize = 20;
